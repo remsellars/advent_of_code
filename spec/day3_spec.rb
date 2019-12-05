68970098f3f0135c04727f74a2ba73e2a5efa144
Joel Vardy <info@joelvardy.com>
2019-12-05 00:24:33 +0000

    day3

--- spec/day3_spec.rb
+++ spec/day3_spec.rb
@@ -13,51 +13,83 @@ RSpec.describe Path do
 end
 
 
-RSpec.describe Day3 do
+RSpec.describe IntersectionCalculator do
 
-  describe ".intersections" do
+  describe "#intersections" do
     specify "R8,U5,L5,D3 and U7,R6,D4,L4 has 2 intersections" do
       path1 = Path.new("R8,U5,L5,D3")
       path2 = Path.new("U7,R6,D4,L4")
-      intersections = Day3.intersections(path1, path2)
-      puts intersections
-      expect(intersections.count).to eq 2
+      calc = IntersectionCalculator.new(path1, path2)
+
+      expect(calc.intersections.count).to eq 2
     end
   end
 
-  describe ".closest_intersection" do
+  describe "#closest_intersection" do
     specify "R8,U5,L5,D3 and U7,R6,D4,L4 have their closest intersect at 3,3" do
       path1 = Path.new("R8,U5,L5,D3")
       path2 = Path.new("U7,R6,D4,L4")
-      intersection = Day3.closest_intersection(path1, path2)
-      puts intersection
+      calc = IntersectionCalculator.new(path1, path2)
+
+      intersection = calc.closest_to_origin
+ 
       expect(intersection.x).to eq 3
       expect(intersection.y).to eq 3
     end
   end
 
-  describe ".closest_intersection_distance" do
+  describe "#closest_intersection distance" do
     specify "R8,U5,L5,D3 and U7,R6,D4,L4 = distance 6" do
       path1 = Path.new("R8,U5,L5,D3")
       path2 = Path.new("U7,R6,D4,L4")
-      expect(Day3.closest_intersection_distance(path1, path2)).to eq 6
+      calc = IntersectionCalculator.new(path1, path2)
+
+      expect(calc.closest_to_origin.distance_from_origin).to eq 6
     end
 
     specify "R75,D30,R83,U83,L12,D49,R71,U7,L72 and U62,R66,U55,R34,D71,R55,D58,R83 = distance 159" do
       path1 = Path.new("R75,D30,R83,U83,L12,D49,R71,U7,L72")
       path2 = Path.new("U62,R66,U55,R34,D71,R55,D58,R83")
-      expect(Day3.closest_intersection_distance(path1, path2)).to eq 159
+      calc = IntersectionCalculator.new(path1, path2)
+
+      expect(calc.closest_to_origin.distance_from_origin).to eq 159
     end
 
     specify "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 and U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = distance 135" do
       path1 = Path.new("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
       path2 = Path.new("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
-      expect(Day3.closest_intersection_distance(path1, path2)).to eq 135
+      calc = IntersectionCalculator.new(path1, path2)
+
+      expect(calc.closest_to_origin.distance_from_origin).to eq 135
     end
   end
 
+  describe "#least_steps" do
+    specify "R75,D30,R83,U83,L12,D49,R71,U7,L72 and U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps" do
+      path1 = Path.new("R75,D30,R83,U83,L12,D49,R71,U7,L72")
+      path2 = Path.new("U62,R66,U55,R34,D71,R55,D58,R83")
+      calc = IntersectionCalculator.new(path1, path2)
+
+      expect(calc.least_steps).to eq 610
+    end
+
+    specify "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 and U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps" do
+      path1 = Path.new("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
+      path2 = Path.new("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+      calc = IntersectionCalculator.new(path1, path2)
+
+      expect(calc.least_steps).to eq 410
+    end
+
+
+  end
+
   specify ".part1" do
-    expect(Day3.part1).to eq 135
+    expect(IntersectionCalculator.part1).to eq 1084
+  end
+
+  specify ".part2" do
+    expect(IntersectionCalculator.part2).to eq 9240
   end
 
 end
